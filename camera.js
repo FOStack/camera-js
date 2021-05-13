327b4a991840a630c93c587368b1dd2f46246e71
Felix O <felix.o.stack@gmail.com>
2021-05-13 15:28:02 -0700

    Update camera.js: Now a module

--- camera.js
+++ camera.js
@@ -1,49 +1,52 @@
-let video = document.createElement('video');
+'use strict';
+moodule.exports = function() {
+    let video = document.createElement('video');
 
-const devices = navigator.mediaDevices || null;
+    const devices = navigator.mediaDevices || null;
 
-function adjust() {
-    video.videoWidth = '100%';
-    video.videoHeight = '100%';
-    video.style.margin = '0 auto';
-    video.style.position = 'absolute';
-    video.style.top = '0vh';
-    video.style.left = '0vw';
-}
-
-function flip(v='environment') { 
-    const constraints = {
-        video: {
-            facingMode: v
-        }
-    };
-    video.style.transform = (v=='user')?'scale(-1, 1)':'';
-    start(constraints);
-}
-
-function start(e=null) {
-    if (devices && devices.getUserMedia) {
-      devices.getUserMedia(e||{ video: true })
-        .then(function (stream) {
-          adjust();
-          video.autoplay = true;
-          video.srcObject = stream;
-          document.body.appendChild(video);
-        })
-        .catch(function (err0r) {
-          console.log("Something went wrong!");
-        });
+    function adjust() {
+        video.videoWidth = '100%';
+        video.videoHeight = '100%';
+        video.style.margin = '0 auto';
+        video.style.position = 'absolute';
+        video.style.top = '0vh';
+        video.style.left = '0vw';
     }
-}
 
-function stop(e=null) {
-  var stream = video.srcObject;
-  var tracks = stream.getTracks();
+    function flip(v='environment') { 
+        const constraints = {
+            video: {
+                facingMode: v
+            }
+        };
+        video.style.transform = (v=='user')?'scale(-1, 1)':'';
+        start(constraints);
+    }
 
-  for (var i = 0; i < tracks.length; i++) {
-    var track = tracks[i];
-    track.stop();
-  }
+    function start(e=null) {
+        if (devices && devices.getUserMedia) {
+          devices.getUserMedia(e||{ video: true })
+            .then(function (stream) {
+              adjust();
+              video.autoplay = true;
+              video.srcObject = stream;
+              document.body.appendChild(video);
+            })
+            .catch(function (err0r) {
+              console.log("Something went wrong!");
+            });
+        }
+    }
 
-  video.srcObject = null;
+    function stop(e=null) {
+      var stream = video.srcObject;
+      var tracks = stream.getTracks();
+
+      for (var i = 0; i < tracks.length; i++) {
+        var track = tracks[i];
+        track.stop();
+      }
+
+      video.srcObject = null;
+    }
 }
